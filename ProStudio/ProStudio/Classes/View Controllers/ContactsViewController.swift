ec4df152dd3c0b8d7309d515f7013a757afe0435
Hadevs <hhadevs@gmail.com>
2018-11-29 21:49:58 +0300

    ыыыс

--- ProStudio/ProStudio/Classes/View Controllers/ContactsViewController.swift
+++ ProStudio/ProStudio/Classes/View Controllers/ContactsViewController.swift
@@ -14,6 +14,7 @@ class ContactsViewController: UIViewController {
 	@IBOutlet weak var label2: UILabel!
 	@IBOutlet weak var label3: UILabel!
     @IBOutlet weak var label0: UILabel!
+    
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		NavigationBarDecorator.decorate(self)
